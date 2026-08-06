9ca4f67bffad6e49b5c0cbc01d5d38a5ea34a371
md-4n4s <m.d.4n4s@gmail.com>
2026-08-06 18:36:58 +0500

    Merge multiple Excel files into a single report

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
 import pandas as pd
+from pathlib import Path
 
-# Read the Excel File
+# ====================
+# Data Cleaning
+# ====================
+
 df = pd.read_excel("products.xlsx")
 
-# Remove duplicate rows based on name
 df = df.drop_duplicates(subset="name", keep="first")
 
 # Remove rows where any data is missing
-df = df.dropna(subset=["name", "review", "price"])
+df = df.dropna()
 
 # Clean and standardize name
 df["name"] = df["name"].str.strip().str.title()
 
-# Save the clean Excel file
-df.to_excel("products.xlsx", index=False)
+df.to_excel("clean_products.xlsx", index=False)
+
+# ====================
+# Manage multiple files
+# ====================
+
+folder = Path("sample_sales_data")
+
+excel_files = folder.glob("*.xlsx")
+
+all_data = []
+for excel_file in excel_files:
+    df = pd.read_excel(excel_file)
+    all_data.append(df)
+
+# Combine all data
+combined_df = pd.concat(all_data, ignore_index=True)
+
+# Drop empty rows
+combined_df = combined_df.dropna()
+
+# Calculate sales by Category
+summary = (
+    combined_df.groupby("Category", as_index=False)["Total Sales ($)"].sum().sort_values("Total Sales ($)", ascending=False)
+)
+
+# Save everything to one Excel workbook
+with pd.ExcelWriter("Sales_Report.xlsx", engine="openpyxl") as writer:
+    combined_df.to_excel(writer, sheet_name = "All Data", index=False)
+    summary.to_excel(writer, sheet_name = "Summary", index=False)
