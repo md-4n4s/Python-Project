7a230018d148d8f01aee7b7c51bfa1f7e7fb1583
md-4n4s <m.d.4n4s@gmail.com>
2026-08-07 15:30:20 +0500

    Style report

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 import pandas as pd
 from pathlib import Path
-
-# ====================
-# Data Cleaning
-# ====================
-
-df = pd.read_excel("products.xlsx")
-
-df = df.drop_duplicates(subset="name", keep="first")
-
-# Remove rows where any data is missing
-df = df.dropna()
-
-# Clean and standardize name
-df["name"] = df["name"].str.strip().str.title()
-
-df.to_excel("clean_products.xlsx", index=False)
-
-# ====================
-# Manage multiple files
-# ====================
+from openpyxl.styles import Font, PatternFill, Border, Side
+from openpyxl.utils import get_column_letter
 
 folder = Path("sample_sales_data")
 
@@ -28,6 +10,7 @@ excel_files = folder.glob("*.xlsx")
 all_data = []
 for excel_file in excel_files:
     df = pd.read_excel(excel_file)
+    df.drop_duplicates(subset="Item ID", keep="first")
     all_data.append(df)
 
 # Combine all data
@@ -45,3 +28,57 @@ summary = (
 with pd.ExcelWriter("Sales_Report.xlsx", engine="openpyxl") as writer:
     combined_df.to_excel(writer, sheet_name = "All Data", index=False)
     summary.to_excel(writer, sheet_name = "Summary", index=False)
+
+    workbook = writer.book
+
+    header_fill = PatternFill(
+        start_color="1F4E78",
+
+        # Used for gradients
+        end_color="1F4E78",
+        fill_type="solid"
+    )
+
+    header_font = Font(
+        bold=True,
+        name="Arial",
+        size=12,
+        color="FFFFFF"
+    )
+
+    thin_border = Border(
+        left = Side(style="thin"),
+        right = Side(style="thin"),
+        top = Side(style="thin"),
+        bottom = Side(style="thin")
+    )
+
+    for sheet in workbook.worksheets:
+
+        for cell in sheet[1]:
+            cell.fill = header_fill
+            cell.font = header_font
+            cell.border = thin_border
+
+        for row in sheet.iter_rows():
+            for cell in row:
+                cell.border = thin_border
+
+        for column in sheet.columns:
+
+            # Maximum length of data in column
+            length = max(
+                len(str(cell.value)) if cell.value else 0
+                for cell in column
+            )
+
+            # Set width of column to length + 3
+            sheet.column_dimensions[
+                get_column_letter(column[0].column)
+            ].width = length + 3
+
+        # Freeze top row and no column
+        sheet.freeze_panes = "A2"
+
+        # Enable Filters
+        sheet.auto_filter.ref = sheet.dimensions
